9a1db578c8494fa75b81b7704ae82994e7326164
Morgan <hx801@nyu.edu>
2021-09-17 11:34:53 -0400

    added custom header

--- app/model.py
+++ app/model.py
@@ -4,12 +4,33 @@ from nba_api.stats.endpoints import commonplayerinfo, shotchartdetail
 class GetInfo:
     @staticmethod
     def getPlayerInfo(playerid):
-        data = commonplayerinfo.CommonPlayerInfo(player_id=playerid).get_response()
+        custom_headers = {
+            'Host': 'stats.nba.com',
+            'Connection': 'keep-alive',
+            'Cache-Control': 'max-age=0',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9',
+        }
+        data = commonplayerinfo.CommonPlayerInfo(player_id=playerid, headers=custom_headers, timeout=100).get_response()
         return data
 
     @staticmethod
     def getShot(playerid, teamid):
+        custom_headers = {
+            'Host': 'stats.nba.com',
+            'Connection': 'keep-alive',
+            'Cache-Control': 'max-age=0',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9',
+        }
+
         data = shotchartdetail.ShotChartDetail(player_id=playerid, team_id=teamid,
                                                season_type_all_star="Regular Season", season_nullable="2015-16",
-                                               context_measure_simple="FGM").get_response()
+                                               context_measure_simple="FGM", headers=custom_headers).get_response()
         return data
